e03f4727f831b92bc82283f4088c0e5d5e2a4168
Jonathan Meyer <Jmeyer@swri.org>
2017-05-11 17:06:33 -0500

    init commit

---
+++ src/torch_cut_qa.cpp
@@ -0,0 +1,8 @@
+#include "cat_laser_scan_qa/torch_cut_qa.h"
+
+cat_laser_scan_qa::TorchCutQAResult
+cat_laser_scan_qa::runQualityAssurance(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud,
+                                       const TorchCutQAParameters &params)
+{
+
+}
